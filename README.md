15b158596f8e71f5d2ce925fa6204dc547c0ec52
Nick Doiron <ndoiron@mapmeld.com>
2016-03-06 19:44:08 -0800

    initial page load

---
+++ README.md
@@ -0,0 +1,10 @@
+# This could be mobile...
+
+## ... but you playing
+
+It's a way to demo what someone else's site will look like
+if they add a mobile-responsive CSS file.
+
+## License
+
+Open source, MIT license
---
+++ app.js
@@ -0,0 +1,59 @@
+
+const koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const convert = require('koa-convert');
+const session = require('koa-generic-session');
+const MongoStore = require('koa-generic-session-mongo');
+const Jade = require('koa-jade');
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const compression = require('koa-compress');
+const csrf = require('koa-csrf');
+const kstatic = require('koa-static');
+
+const mongoose = require('mongoose');
+const thunkify = require('thunkify');
+const request = require('koa-request');
+
+console.log('Connecting to MongoDB (required)');
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'localhost');
+mongoose.connection.on("error", function(err) {
+  console.log(err);
+});
+
+var app = koa();
+const jade = new Jade({
+  viewPath: './views'
+});
+app.use(jade.middleware);
+
+app.use(kstatic(__dirname + '/static'));
+app.use(bodyParser());
+app.use(compression());
+//app.use(cookieParser());
+
+app.keys = ['wkpow3jocijoid3jioj3', 'cekopjpdjjo3jcjio3jc'];
+app.use(session({
+  store: new MongoStore()
+}));
+
+// app.use(logger());
+csrf(app);
+app.use(csrf.middleware);
+
+// routes
+router.get('/', function* () {
+  this.render('home');
+})
+.get('/page', function* () {
+  // console.log(this.request.query);
+  var response = yield request(this.request.query.url);
+  this.body = response.body.replace('</head>', '<style type="text/css">body, div { color: red !important; }</style></head>');
+});
+
+app.use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(process.env.PORT || 8080);
+
+module.exports = app;
